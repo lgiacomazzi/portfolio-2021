a547040b545d77574fc18ea07187a5764505c288
DM Brasil <dmbrasil@MacBook-Pro-de-Infraestrutura-4.local>
2022-05-17 15:04:52 -0300

    aleluia pronto pro merge

--- components/Manifesto.tsx
+++ components/Manifesto.tsx
@@ -1,23 +1,22 @@
 import styles from "../styles/components/Manifesto.module.css";
 import { motion } from "framer-motion";
 import Button from "../components/Button";
-import { Box, Section } from "../components/Box";
 
 
 export default function Manifesto() {
   return (
-    <Section className={styles.manifesto} pt={120} pb={120}>
-      <Box column gap={16}>
+    <div className={styles.manifesto}>
+      <div className="container">
         <motion.h1 className={styles.manifestoText}>
           hey there, i'm luciano — a product designer experienced in frontend development. Currently solving complex design challenges at Delivery Much.
         </motion.h1>
 
-        <Box row gap={8}>
+        <div className="d-flex flex-row gap-8">
           <Button>Portfólio</Button>
           <Button>Sobre mim</Button>
           <Button>Contato</Button>
-        </Box>
-      </Box>
-    </Section>
+        </div>
+      </div>
+    </div>
   );
 }
