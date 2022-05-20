2d73c834325ed41f6e19b4579c5d38afdff139b0
DM Brasil <dmbrasil@MacBook-Pro-de-Infraestrutura-4.local>
2022-05-20 09:46:12 -0300

    add change dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,30 @@ import "../styles/bootstrap.css";
 import "../styles/_globals.css";
 
 import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState('light');
 
-  useEffect(() => {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
+  const handleThemeChange = (e) => {
+    console.log("handle change start")
+    if (e.matches) {
       document.documentElement.classList.add('dark')
     } else {
       document.documentElement.classList.remove('dark')
     }
-  }, [])
+
+  }
+
+  useEffect(() => {
+    console.log("useEffect start")
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    handleThemeChange(darkModeMediaQuery);
+    darkModeMediaQuery.addEventListener("change", handleThemeChange)
+    return () => {
+      darkModeMediaQuery.removeEventListener('change', handleThemeChange)
+    }
+  })
 
 
   return (
