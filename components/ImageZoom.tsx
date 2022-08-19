7ce4a3e7a5e76d7e24cd6b78934e27efa9254c81
DM Brasil <dmbrasil@MacBook-Pro-de-Infraestrutura-4.local>
2022-08-19 18:13:17 -0300

    avancos no zoom

--- components/ImageZoom.tsx
+++ components/ImageZoom.tsx
@@ -1,22 +1,33 @@
 
 import styles from "../styles/components/ImageZoom.module.css";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+
+const variants = {
+    open: { opacity: 1, scale: 1 },
+    closed: { opacity: 0.8, scale: .5 },
+}
 
 export default function ImageZoom({src,alt}) {
   const [isOpen, setOpen] = useState(false);
 
   return (
     <div className={isOpen ? styles.imageContainerOpen : styles.imageContainer}>
-        { isOpen &&
+        {isOpen && 
+        <AnimatePresence>
             <motion.div
+                animate="open"
+                initial="closed"
+                variants={variants}
                 className={styles.imageOpen}
                 onClick={() => setOpen(!isOpen)}>
                     {/* Imagem com Zoom */}
-                    <img src={src} alt={alt}/>
+                    <div>
+                        <img src={src} alt={alt}/>
+                    </div>
                     <p className="text-body">{alt}</p>
             </motion.div> 
-        }
+        </AnimatePresence> }
         {/* Imagem do documento */}
         <img src={src} alt={alt} onClick={() => setOpen(!isOpen)}/>
         <p className="text-center text-small text-secondary mb-3">{alt}</p>
