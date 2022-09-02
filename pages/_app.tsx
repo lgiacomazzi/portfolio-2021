9aebecc65b5124bfd2c5163facac69b1aff9ceff
DM Brasil <dmbrasil@MacBook-Pro-de-Infraestrutura-4.local>
2022-09-02 10:03:39 -0300

    add ga tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,35 @@ import ThemeContextProvider from "../contexts/ThemeContext";
 import Header from "../components/Header";
 import { Footer } from "../components/Footer";
 import { AnimatePresence } from "framer-motion";
-
+import Script from 'next/script';
 import { NextIntlProvider } from 'next-intl'; // 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeContextProvider>
-      <NextIntlProvider messages={pageProps.messages}>
-        <Header />
-        <AnimatePresence exitBeforeEnter>
-          <Component {...pageProps} />
-        </AnimatePresence>
-        <Footer />
-      </NextIntlProvider>
-    </ThemeContextProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0H7QWYBQYN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0H7QWYBQYN', { 'debug_mode':true });
+        `}
+      </Script>
+      <ThemeContextProvider>
+        <NextIntlProvider messages={pageProps.messages}>
+          <Header />
+          <AnimatePresence exitBeforeEnter>
+            <Component {...pageProps} />
+          </AnimatePresence>
+          <Footer />
+        </NextIntlProvider>
+      </ThemeContextProvider>
+    </>
   );
 }
 
